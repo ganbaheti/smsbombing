b1813a969c5b39ee79bda46cb62e7bddf7f3fe0c
GAGAN BAHETI <gaganbaheti99@gmail.com>
2019-12-22 14:04:41 +0530

    create final.py

---
+++ final.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+URL = 'https://www.sms4india.com/api/v1/sendCampaign'
+
+# for getting request the code is : 
+def sendPostRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textMessage):
+  req_params = {
+  'apikey':apiKey,
+  'secret':secretKey,
+  'usetype':useType,
+  'phone': phoneNo,
+  'message':textMessage,
+  'senderid':senderId
+  }
+  return requests.post(reqUrl, req_params)
+
+# for response
+response = sendPostRequest(URL, 'api key', 'secret key', 'for testing put: stage', 'mobile no.', 'email id', 'text message' )
